176bfe8b9d8e08d5f3547d76546ab4e06613b6b4
Razvan Tache <rtache@tremend.ro>
2017-01-31 22:58:16 +0200

    Add additional tests for the complex puzzle

--- tests/app/tests/SudokuSolverTest.php
+++ tests/app/tests/SudokuSolverTest.php
@@ -41,16 +41,19 @@ class SudokuSolverTest extends \PHPUnit_Framework_TestCase
     /**
      * Tests a complex puzzle.
      * A complex puzzle is a sudoku which can have more than one option per empty cell.
+     * @param array $puzzle
+     * @param array
+     * @dataProvider complexPuzzleDataProvider
      */
-    public function testComplexPuzzle()
+    public function testComplexPuzzle($puzzle, $expectedSolution)
     {
-        $sudokuSolver = new SudokuSolver($this->getComplexPuzzle());
+        $sudokuSolver = new SudokuSolver($puzzle);
 
         $success = $sudokuSolver->solve();
         $solution = $sudokuSolver->getSolution();
 
         $this->assertTrue($success);
-        $this->assertEquals($this->getComplexPuzzleSolution(), $solution);
+        $this->assertEquals($expectedSolution, $solution);
     }
 
     /**
@@ -111,10 +114,23 @@ class SudokuSolverTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     * Data provider for the testComplexPuzzle method.
+     *
+     * @return array
+     */
+    public function complexPuzzleDataProvider()
+    {
+        return [
+            [$this->getComplexPuzzle1(), $this->getComplexPuzzle1Solution()],
+            [$this->getComplexPuzzle2(), $this->getComplexPuzzle2Solution()],
+        ];
+    }
+
+    /**
      * Returns a complex sudoku puzzle.
      * @return array
      */
-    private function getComplexPuzzle()
+    private function getComplexPuzzle1()
     {
         return [
             [0, 2, 3, 4, 0, 0, 0, 0, 9,],
@@ -133,7 +149,7 @@ class SudokuSolverTest extends \PHPUnit_Framework_TestCase
      * Returns the solution for the complex puzzle.
      * @return array
      */
-    private function getComplexPuzzleSolution()
+    private function getComplexPuzzle1Solution()
     {
         return [
             [1, 2, 3, 4, 5, 6, 7, 8, 9,],
@@ -145,6 +161,44 @@ class SudokuSolverTest extends \PHPUnit_Framework_TestCase
             [3, 1, 2, 6, 4, 5, 9, 7, 8,],
             [6, 4, 5, 9, 7, 8, 3, 1, 2,],
             [9, 7, 8, 3, 1, 2, 6, 4, 5,],
+        ];
+    }
+
+    /**
+     * Return the second complex puzzle to be tested.
+     * @return array
+     */
+    private function getComplexPuzzle2()
+    {
+        return [
+            [5,3,0,0,7,0,0,0,0],
+            [6,0,0,1,9,5,0,0,0],
+            [0,9,8,0,0,0,0,6,0],
+            [8,0,0,0,6,0,0,0,3],
+            [4,0,0,8,0,3,0,0,1],
+            [7,0,0,0,2,0,0,0,6],
+            [0,6,0,0,0,0,2,8,0],
+            [0,0,0,4,1,9,0,0,5],
+            [0,0,0,0,8,0,0,7,9],
+        ];
+    }
+
+    /**
+     * Returns the solution for the second complex puzzle.
+     * @return array
+     */
+    private function getComplexPuzzle2Solution()
+    {
+        return [
+            [5,3,4,6,7,8,9,1,2],
+            [6,7,2,1,9,5,3,4,8],
+            [1,9,8,3,4,2,5,6,7],
+            [8,5,9,7,6,1,4,2,3],
+            [4,2,6,8,5,3,7,9,1],
+            [7,1,3,9,2,4,8,5,6],
+            [9,6,1,5,3,7,2,8,4],
+            [2,8,7,4,1,9,6,3,5],
+            [3,4,5,2,8,6,1,7,9],
         ];
     }
 }
